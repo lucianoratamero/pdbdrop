22491699fbf69221dc2be3651f329f1c1ee64d18
Luciano Ratamero <luciano@ratamero.com>
2017-05-20 19:05:38 -0300

    adds validator to the res field.

--- pdbdrop/forms.py
+++ pdbdrop/forms.py
@@ -8,6 +8,11 @@ class UploadForm(forms.ModelForm):
     upload_file = forms.FileField()
     video = forms.FileField(required=False)
 
+    def clean_res(self):
+        res_data = self.cleaned_data['res']
+        if res_data and len(str(res_data)) < 3:
+            raise forms.ValidationError('You need to pass at least two parameters to the res field')
+
     class Meta:
         model = Upload
         exclude = ['file_path', 'video_path']
